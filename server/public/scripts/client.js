f7961b6a0ef621d9a09d5a447e6a86d20f823328
Laney McKee <laneymckeephotography@gmail.com>
2019-08-03 22:23:00 -0500

    client functions set up, ready to send to server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,35 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  console.log("running js");
+    console.log('running js');
+    $('.operator').on('click', setOperator)
+    $('#calculate').on('click', calculateAnswer)
+    $('#clear').on('click', clearInputs);
+}
+
+let calculations = [];
+let operator = 'none';
+
+function setOperator() {
+    console.log('setting operator...');
+    operator = $(this).text();
+    console.log(operator);
+}
+
+function calculateAnswer() {
+    console.log('calculating answer...');
+    let values = {
+        numberOne: $('#valueOne').val(),
+        numberTwo: $('#valueTwo').val(),
+        operation: operator
+    }
+    calculations.push(values)
+    console.log(calculations);
+
+}
+
+function clearInputs() {
+    $('#valueOne').val('');
+    $('#valueTwo').val('');
+    operator = 'none';
 }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
+    console.log('Server is running on port', PORT);
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
