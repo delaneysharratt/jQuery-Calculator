75122eaa18b8579051513f3dff724d0ce45a1ec0
Laney McKee <laneymckeephotography@gmail.com>
2019-08-03 23:35:19 -0500

    calculator functioning, working on history and display

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,24 +10,54 @@ function handleReady() {
 let calculations = [];
 let operator = 'none';
 
+//setting operator for equation (+ - * /)
 function setOperator() {
     console.log('setting operator...');
     operator = $(this).text();
     console.log(operator);
 }
 
+//getting answers back from server
+function answer() {
+    console.log('answer is...');
+    //get messages from server -- AJAX!
+    $.ajax({
+        method: "GET",
+        url: "/answers"
+    }).then(function (response) {
+        console.log("the answer is:", response);
+        renderToDom(response);
+    });
+}
+
+//creating object from inputs + sending to server
 function calculateAnswer() {
     console.log('calculating answer...');
     let values = {
         numberOne: $('#valueOne').val(),
-        numberTwo: $('#valueTwo').val(),
-        operation: operator
+        operation: operator,
+        numberTwo: $('#valueTwo').val()
     }
     calculations.push(values)
     console.log(calculations);
 
+    $.ajax({
+        method: "POST",
+        url: "/calculate",
+        data: values
+    }).then(function (response) {
+        console.log(response);
+        answer();
+    });
+
 }
 
+//rendering the equation to the DOM
+function renderToDom(equations) {
+    console.log("rendering...");
+}
+
+//clearing the two input values
 function clearInputs() {
     $('#valueOne').val('');
     $('#valueTwo').val('');
--- server/server.js
+++ server/server.js
@@ -3,10 +3,48 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = 5000;
 
+let equations = [];
+
 app.listen(PORT, () => {
     console.log('Server is running on port', PORT);
 });
 
+//serves back files in public folder
+// (html, css, js, images, etc)
+app.use(express.static("server/public"));
+//body parser setup
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+//returns updated equation objects with answers to client
+app.get("/answers", (req, res) => {
+    // res.sendStatus(200);
+    console.log('sending answers');
+    res.send(equations);
+});
+
+app.post('/calculate', (req, res) => {
+    console.log('calculating in post', req.body);
+    //store the item in our inventory
+    let input = req.body;
+    equations.push(input);
+    for (let input of equations) {
+        if (input.operation == '+') {
+            console.log('adding...');
+            input.answer = Number(input.numberOne) + Number(input.numberTwo);
+        } else if (input.operation == '-') {
+            console.log('subtracting..');
+            input.answer = input.numberOne - input.numberTwo;
+        } else if (input.operation == '*') {
+            console.log('multiplying..');
+            input.answer = input.numberOne * input.numberTwo;
+        } else if (input.operation == '/') {
+            console.log('dividing..');
+            input.answer = input.numberOne / input.numberTwo;
+        }
+    }
+    console.log('updated calculations', equations);
+    //all good servers respond!
+    res.sendStatus(201); //CREATED
+});
