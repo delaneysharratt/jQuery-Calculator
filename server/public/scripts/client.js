8e871c73d4f70000db5da09db14b64ed980dee43
Laney McKee <laneymckeephotography@gmail.com>
2019-08-05 22:33:20 -0500

    finalizing functionality for challenge

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,106 +1,112 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-    console.log('running js');
-    $('.numberBtn').on('click', displayEquation)
-    $('.operator').on('click', setNumberOne)
-    $('.operator').on('click', setOperator)
-    $('#calculate').on('click', setNumberTwo)
-    $('#calculate').on('click', calculateAnswer)
-    $('#clear').on('click', clearInputs);
+  console.log("running js");
+  $(".numberBtn").on("click", displayEquation);
+  $(".operator").on("click", setOperator);
+  $("#calculate").on("click", calculateAnswer);
+  $("#clear").on("click", clearInputs);
+  answer();
 }
 
+let calculations = [];
 let equation = [];
-let equationDisplay = '';
-let numberOne = '';
-let numberTwo = '';
-let operator = '';
+let equationDisplay = "";
+let numberOne = "";
+let numberTwo = "";
+let operator = "";
 
 //setting operator for equation (+ - * /)
 function setOperator() {
-    console.log('setting operator...');
-    operator = $(this).text();
-    equationDisplay += operator;
-    $('#equationDisplay').val(equationDisplay);
+  setNumberOne();
+  console.log("setting operator...");
+  operator = $(this).text();
+  equationDisplay += operator;
+  $("#equationDisplay").val(equationDisplay);
 }
 
 //setting numberOne w/ current values in input display
 function setNumberOne() {
-    numberOne = equation.join('');
-    console.log('#1:', numberOne);
-    equation = [];
+  numberOne = equation.join("");
+  console.log("#1:", numberOne);
+  equation = [];
 }
 
 //setting numberTwo w/ input values after operator set
 function setNumberTwo() {
-    numberTwo = equation.join('');
-    console.log('#2:', numberTwo);
-    equation = [];
-    console.log(equationDisplay);
-
+  numberTwo = equation.join("");
+  console.log("#2:", numberTwo);
+  equation = [];
+  console.log(equationDisplay);
 }
 
 //adds each button clicked into input display
 function displayEquation() {
-    let digit = $(this).text();
-    equation.push(digit);
-    equationDisplay += digit;
-    $('#equationDisplay').val(equationDisplay);
+  let digit = $(this).text();
+  equation.push(digit);
+  equationDisplay += digit;
+  $("#equationDisplay").val(equationDisplay);
 }
 
 //getting answers back from server
 function answer() {
-    console.log('answer is...');
-    //get messages from server -- AJAX!
-    $.ajax({
-        method: 'GET',
-        url: '/answers'
-    }).then(function (response) {
-        console.log(response);
-        renderToDom(response);
-    });
+  //get messages from server -- AJAX!
+  $.ajax({
+    method: "GET",
+    url: "/answers"
+  }).then(function(response) {
+    renderToDom(response);
+  });
 }
 
 //creating object from inputs + sending to server
 function calculateAnswer() {
-    console.log('calculating answer...');
-    let values = {
-        numberOne: numberOne,
-        operation: operator,
-        numberTwo: numberTwo
-    }
+  setNumberTwo();
+  console.log("calculating answer...");
 
-    //sending equation for calculation in server
-    $.ajax({
-        method: 'POST',
-        url: '/calculate',
-        data: values
-    }).then(function (response) {
-        console.log(response);
-        answer();
-    });
-
+  if (operator == 0 || numberOne == 0 || numberTwo == 0) {
+    alert("Equation incomplete, please enter all components.");
+    return false;
+  }
+  let values = {
+    numberOne: numberOne,
+    operation: operator,
+    numberTwo: numberTwo
+  };
+  //sending equation for calculation in server
+  $.ajax({
+    method: "POST",
+    url: "/calculate",
+    data: values
+  }).then(function(response) {
+    console.log(response);
+    answer();
+  });
+  clearInputs();
 }
 
 //rendering the equation to the DOM
 function renderToDom(equations) {
-    console.log('rendering...');
-    $('#answerLog').empty();
+  console.log("rendering...");
+  $("#answerLog").empty();
 
-    for (let input of equations) {
-        $('#answer').html(input.answer);
+  for (let input of equations) {
+    $("#answer").html(input.answer);
 
-        $('#answerLog').prepend(
-            `<li>${input.numberOne} ${input.operation} ${input.numberTwo} = ${input.answer}</li>`
-        );
-    }
+    $("#answerLog").prepend(
+      `<li>${input.numberOne} ${input.operation} ${input.numberTwo} = ${
+        input.answer
+      }</li>`
+    );
+  }
 }
 
 //clearing all values for next calculation
 function clearInputs() {
-    $('#equationDisplay').val('');
-    equationDisplay = '';
-    numberOne = '';
-    numberTwo = '';
-    operator = 'none';
+  $("#equationDisplay").val("");
+  equationDisplay = "";
+  equation = [];
+  numberOne = "";
+  numberTwo = "";
+  operator = "none";
 }
--- server/server.js
+++ server/server.js
@@ -1,55 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const PORT = 5000;
 
 let equations = [];
 
 app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
+  console.log("Server is running on port", PORT);
 });
 
 //serves back files in public folder
 // (html, css, js, images, etc)
 app.use(express.static("server/public"));
 //body parser setup
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 
 //returns updated equation objects with answers to client
 app.get("/answers", (req, res) => {
-    // res.sendStatus(200);
-    console.log('sending answers');
-    res.send(equations);
+  // res.sendStatus(200);
+  console.log("sending answers");
+  res.send(equations);
 });
 
 //calculations in post using inputs from get
-app.post('/calculate', (req, res) => {
-    console.log('calculating in post', req.body);
-    let input = req.body;
-    //store the updated input in equations
-    equations.push(input);
+app.post("/calculate", (req, res) => {
+  console.log("calculating in post", req.body);
+  let input = req.body;
 
+  if (input.operation == "+") {
+    console.log("adding...");
+    input.answer = Number(input.numberOne) + Number(input.numberTwo);
+  } else if (input.operation == "-") {
+    console.log("subtracting..");
+    input.answer = input.numberOne - input.numberTwo;
+  } else if (input.operation == "*") {
+    console.log("multiplying..");
+    input.answer = input.numberOne * input.numberTwo;
+  } else if (input.operation == "/") {
+    console.log("dividing..");
+    input.answer = input.numberOne / input.numberTwo;
+  }
 
-    for (let input of equations) {
-        if (input.operation == '+') {
-            console.log('adding...');
-            input.answer = Number(input.numberOne) + Number(input.numberTwo);
-        } else if (input.operation == '-') {
-            console.log('subtracting..');
-            input.answer = input.numberOne - input.numberTwo;
-        } else if (input.operation == '*') {
-            console.log('multiplying..');
-            input.answer = input.numberOne * input.numberTwo;
-        } else if (input.operation == '/') {
-            console.log('dividing..');
-            input.answer = input.numberOne / input.numberTwo;
-        }
-    }
+  //store the updated input in equations
+  equations.push(input);
 
-
-    console.log('updated calculations', equations);
-    //all good servers respond!
-    res.sendStatus(201); //CREATED
+  console.log("updated calculations", equations);
+  //all good servers respond!
+  res.sendStatus(201); //CREATED
 });
